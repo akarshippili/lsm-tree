4fb8fdc8cfc65a218bec4a4130edb3df0709ab97
Ippili Akarsh <65852816+akarshippili@users.noreply.github.com>
2026-09-19 22:13:41 +0530

    Update main.go

--- test/main.go
+++ test/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	lsmtree "github.com/akarshippili/lsm-tree"
 )
 
 func main() {
+	logLevel := flag.String("log-level", "info", "log level: debug, info, warn, error")
+	flag.Parse()
+
+	switch *logLevel {
+	case "debug":
+		lsmtree.SetLogLevel(lsmtree.DEBUG)
+	case "info":
+		lsmtree.SetLogLevel(lsmtree.INFO)
+	case "warn":
+		lsmtree.SetLogLevel(lsmtree.WARN)
+	case "error":
+		lsmtree.SetLogLevel(lsmtree.ERROR)
+	}
 
 	entries := []lsmtree.Entry{}
 	for i := 0; i < 100; i++ {
