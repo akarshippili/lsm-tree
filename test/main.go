a337606dad24204d5bb671c82a0a073dfb0b6a7c
Ippili Akarsh <65852816+akarshippili@users.noreply.github.com>
2026-09-20 17:37:44 +0530

    Update main.go

--- test/main.go
+++ test/main.go
@@ -23,7 +23,7 @@ func main() {
 	}
 
 	entries := []lsmtree.Entry{}
-	for i := 0; i < 100; i++ {
+	for i := range(100) {
 		entries = append(entries, lsmtree.Entry{Key: fmt.Sprintf("key-%d", i), Value: fmt.Sprintf("value-%d", i)})
 	}
 
